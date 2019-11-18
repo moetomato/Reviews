5bb7f5851c0aacf2734b4d0ed224c89bf0d70586
Moeko Taniguchi <maronmaron61293@gmail.com>
2019-11-18 14:37:11 +0900

    add func

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -7,7 +7,8 @@ contract Main {
 
     uint constant numOfAllCheckers = 100;
 
-    string lastCalcTimeStamp;
+    // 11/17 -> 1117
+    uint lastCalcTimeStamp;
 
   struct Review {
       string reviewer;
@@ -24,13 +25,15 @@ contract Main {
 
   struct Reviewer {
     string name;
-    address payable adr;
+    address adrs;
+    // initiated with 100.
     uint score;
+    uint newScore;
   }
 
   struct Reviewee {
     string name;
-    address adr;
+    address adrs;
     uint score;
   }
 
@@ -38,8 +41,10 @@ contract Main {
     address payable adrs;
   }
 
-  bool[numOfReviewees][] public graph;
+  string[] private reviewers;
 
+  mapping(string => bool) reviewerHasPR;
+ 
   Checker[] public  checkers;
 
   mapping (string => Reviewer) nameToReviewer;
@@ -50,13 +55,30 @@ contract Main {
 
   mapping(string => Review[]) reviewerToReview;
 
-  constructor() public {
-    graph = new bool[numOfReviewees][](numOfReviewees);
-  }
+  mapping(string => string[]) graph;
+
+  // constructor() public {
+  // }
 
   event notifyChecker(address indexed adr, string reviewer, uint num);
 
   event checkDone(string indexed userName, uint num);
+
+  function _addReviewer(string memory _name, address _adrs) public {
+    Reviewer memory reviewer = Reviewer(_name, _adrs, 100, 0);
+    reviewers.push(_name);
+    nameToReviewer[_name] = reviewer;
+  }
+
+  function _addReviewee(string memory _name, address _adrs) public {
+    Reviewee memory reviewee = Reviewee(_name,_adrs, 0);
+    nameToReviewee[_name] = reviewee;
+  }
+
+  function _addChecker(address payable _adrs) public {
+    Checker memory checker = Checker(_adrs);
+    checkers.push(checker);
+  }
 
   function _generateRandomNumber(string memory _str) private pure returns (uint) {
       uint rand = uint(keccak256(abi.encode(_str)));
@@ -100,6 +122,10 @@ contract Main {
 
   function _addApproviedReview(string memory _reviewer, uint _num) public payable {
     Review storage review = reviewerToReview[_reviewer][_num];
+    Reviewee memory reviewee = nameToReviewee[review.reviewee];
+    uint numOfReview = revieweeToReview[reviewee.name].length;
+    uint newScore = (reviewee.score * numOfReview + review.score) / (numOfReview + 1);
+    reviewee.score = newScore;
     revieweeToReview[review.reviewee].push(review);
     if(review.checkerToApproval[review.checker1.adrs] == review.isApproved) _send(review.checker1.adrs, msg.value);
     if(review.checkerToApproval[review.checker2.adrs] == review.isApproved) _send(review.checker2.adrs, msg.value);
@@ -108,5 +134,29 @@ contract Main {
 
   function _send(address payable _to, uint amount) public {
     _to.transfer(amount);
+  }
+
+  function _addPeerReview(string memory _from, string memory _to, uint _timeStamp) public {
+    graph[_from].push(_to);
+    reviewerHasPR[_from] = true;
+    if(lastCalcTimeStamp != _timeStamp){
+      _updateScore();
+    }
+  }
+
+  function _updateScore() private {
+    for(uint i = 0; i < reviewers.length; i++){
+      if(reviewerHasPR[reviewers[i]] == false) continue;
+      string[] memory rs = graph[reviewers[i]];
+      for(uint j = 0; j<rs.length; j++){
+        Reviewer storage r2 = nameToReviewer[rs[j]];
+        r2.newScore = r2.newScore + nameToReviewer[reviewers[i]].score / rs.length;
+      }
+    }
+    for(uint i = 0; i < reviewers.length; i++){
+      Reviewer storage reviewer = nameToReviewer[reviewers[i]];
+      reviewer.score = reviewer.newScore;
+      reviewer.newScore = 0;
+    }
   }
 }
